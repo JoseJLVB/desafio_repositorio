f2dac96b9321fc6dd746d953fdee0493cb88346b
Jose Luis Valcarcel Bravo <josevalcarcel@entrearaucarias.com>
2021-12-30 20:33:46 -0300

    api contactada

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,22 @@ async function getUser(username) {
     `)
 };
 
+async function getRepo(username, page, perpage) {
+    const repos = await request(`${username}/repos?page=${page}$per_page=${perpage}`);
+    $("#resultadosRight").html(`
+    <div clas="col-6 text-right">
+    <h1>Nombre de Repositorios</h1>
+        
+    </div>
+    `)
+};
+
+async function request(path) {
+    try {
+        const datos = await fetch(`https://api.github.com/users/${path}`);
+        const retorno = await datos.json();
+        return retorno
+    }catch(error){
+        console.log(error.message)
+    }
+};
