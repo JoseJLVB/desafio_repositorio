7c3edf24e8f84a71fdfcbbc82e5b3d9060cfcee7
Jose Luis Valcarcel Bravo <josevalcarcel@entrearaucarias.com>
2021-12-30 17:13:00 -0300

    Prevent Default, y variables creadas

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+$("form").on("submit", function(ev){
+    ev.preventDefault();
+    const nombre = $('#nombre').val();
+    const pagina = $('#pagina').val();
+    const repoPagina = $('#repoPagina').val();
+    
+})
+
